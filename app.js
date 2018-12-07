191ff5f18c309ca6b0bb17d956f9c8a0b8604e34
hamdon <hamdongunner@gmail.com>
2018-12-07 19:38:15 +0300

    before task

--- app.js
+++ app.js
@@ -7,6 +7,8 @@
  const url = require('url');
  const StringDecoder = require('string_decoder').StringDecoder;
 
+
+
 // Create a server so it can deal with our requestes
  let server = http.createServer((req, res) => {
 
@@ -33,18 +35,69 @@
 
    let buffer = '';
 
+   // When the request start sending data (payload)
    req.on('data', (data) => {
      buffer += decoder.write(data);
-     console.log(buffer);
    });
 
+   // When the request stop sending data (payload)
    req.on('end', () => {
      buffer += decoder.end();
-     console.log(buffer);
-     res.end('Hello World');
-   });
 
+     // Choosing a function that should be called
+     let chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
+
+     // Setting the data we have got from the requesr so we can send them to the chosenHandler
+     let data = {
+       'method': method,
+       'query': queryObject,
+       'payload': buffer,
+       'headers': headers
+     }
+
+     // Calling the chosenHandler
+     chosenHandler(data, (statusCode, returnedData) => {
+
+       // Handling the casr when the below function does not send a valid status code
+       statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
+
+       // Handling the casr when the below function does not send a valid object
+       returnedData = typeof(returnedData) == 'object' ? returnedData : {};
+
+       // Coverting the object to a string
+       let stringPayload = JSON.stringify(returnedData);
+
+       res.setHeader('Content-Type','application/json');
+
+       // Returning the status code in the header
+       res.writeHeader(statusCode);
+
+       // Returning the information in the header
+       res.end(stringPayload);
+     });
+   });
  });
+
+
+let handlers = {};
+
+handlers.home = function (data, callback) {
+  callback(200, {'location':'home'});
+};
+
+handlers.users = function (data, callback) {
+  callback(200, {'location':'users'});
+};
+
+handlers.notFound = function (data, callback) {
+  callback(404);
+};
+
+let router = {
+  'home': handlers.home,
+  'users': handlers.users
+};
+
 
 
 // Listen on a port
